c6268a8976170b781dd82bd4ebf75328b3378843
Dimas Firmansyah <deirn@bai.lol>
2025-03-19 13:58:27 +0700

    proxy prisma studio

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,16 @@
-import path from 'node:path'
-import process from 'node:process'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import express from 'express'
-import swaggerUi from 'swagger-ui-express'
+import { setupDevRoutes } from './dev'
 import { authErrorMiddleware } from './middleware/auth'
 import { dataMiddleware } from './middleware/data'
 import { errorMiddleware } from './middleware/error'
 import { RegisterRoutes } from './routes/routes'
-import * as swaggerJson from './routes/swagger.json'
-import { isDev } from './utils'
+import { isDev, port } from './utils'
 
 dotenv.config()
 
 const app = express()
-const port = process.env.PORT || 3000
 
 // TODO: Configure CORS
 app.use(cors())
@@ -23,18 +19,8 @@ app.use(dataMiddleware)
 
 RegisterRoutes(app)
 
-if (isDev) {
-  app.use('/docs', express.static('docs'))
-
-  app.get('/docs/api.json', (_, res) => {
-    res.sendFile(path.join(__dirname, 'routes/swagger.json'))
-  })
-  app.use('/docs/api', swaggerUi.serve, swaggerUi.setup(swaggerJson, {
-    swaggerOptions: {
-      persistAuthorization: true,
-    },
-  }))
-}
+if (isDev)
+  setupDevRoutes(app)
 
 app.use(authErrorMiddleware)
 app.use(errorMiddleware)
