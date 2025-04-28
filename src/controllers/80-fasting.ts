d2526ed54a638d62ca5c5dd748e9f36dac91b255
Dimas Firmansyah <deirn@bai.lol>
2025-04-28 09:43:36 +0700

    up

--- src/controllers/80-fasting.ts
+++ src/controllers/80-fasting.ts
@@ -1,18 +1,25 @@
+import type { FastingEntry } from '@prisma/client'
 import type { Api, UUID } from '../api.js'
 import type { FastingCategory } from '../db.js'
 import type { AuthRequest } from '../middleware/auth.js'
-import { Body, Controller, Get, Middlewares, Path, Post, Request, Route, Security, Tags } from 'tsoa'
+import { Body, Controller, Delete, Get, Middlewares, Path, Post, Query, Request, Route, Security, Tags } from 'tsoa'
 import { err, ok } from '../api.js'
-import { db } from '../db.js'
+import { cleanUpdateAttrs, db } from '../db.js'
 import { roleMiddleware } from '../middleware/role.js'
 import { verifiedMiddleware } from '../middleware/verified.js'
 import { enqueueWork } from '../worker/queue.js'
 
 interface FastingData {
+  id?: UUID
   category: FastingCategory
   startTime: Date
   endTime: Date
   finishedAt: Date | null
+}
+
+function clean(res: FastingEntry): FastingData {
+  const { userId, ...rest } = cleanUpdateAttrs(res)
+  return rest
 }
 
 @Route('fasting')
@@ -78,6 +85,47 @@ export class FastingController extends Controller {
     await db.fastingEntry.update({
       where: { id, userId, finishedAt: null, deletedAt: null },
       data: { finishedAt: new Date() },
+    })
+
+    return ok()
+  }
+
+  /** Get a list of journal entries. */
+  @Get('/journal')
+  public async getFastingJournals(
+    @Request() req: AuthRequest,
+    @Query() after?: UUID,
+  ): Api<FastingData[]> {
+    const userId = req.user!.id
+
+    const res = after
+      ? await db.fastingEntry.findMany({
+        take: 10,
+        skip: 1,
+        cursor: { id: after },
+        where: { userId },
+        orderBy: { createdAt: 'asc' },
+      })
+      : await db.fastingEntry.findMany({
+        take: 10,
+        where: { userId },
+        orderBy: { createdAt: 'asc' },
+      })
+
+    return ok(res.map(clean))
+  }
+
+  /** Delete a journal entry. */
+  @Delete('/journal/id/{id}')
+  public async deleteFastingJournal(
+    @Request() req: AuthRequest,
+    @Path() id: UUID,
+  ): Api {
+    const userId = req.user!.id
+
+    await db.fastingEntry.update({
+      where: { id, userId, deletedAt: null },
+      data: { deletedAt: new Date() },
     })
 
     return ok()
